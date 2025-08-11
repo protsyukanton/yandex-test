ff3aae1680c6c8e6f4616dfabc510e59501ea234
Антон <anton@MacBook-Air-Anton.local>
2025-08-11 11:32:23 +0300

    add: add running-line

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,5 +1,14 @@
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', () => {
 
-    
+    const textWidth = document.querySelector('.running-line__item').scrollWidth,
+        duration = textWidth / 50,
+        tl = gsap.timeline({ repeat: -1 });
+
+    tl.to('.running-line__item', {
+        x: -textWidth,
+        duration: duration,
+        ease: "none",
+        repeat: -1,
+    });
 
 });
